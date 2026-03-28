294e581101fc792e71297e7ee8104df4be306a43
mishacecior-art <mishacecior@gmail.com>
2026-03-28 17:46:26 +0300

    Update program.py

--- program.py
+++ program.py
@@ -3,6 +3,7 @@ def main():
     levels = []
 
     while True:
+        print("\n")
         print("1. Добавить игрока")
         print("2. Показать всех игроков")
         print("3. Удалить игрока")
@@ -10,6 +11,7 @@ def main():
         print("5. Повысить уровень")
         print("6. Сортировка игроков")
         print("7. Выход")
+        print()
 
         choice = input("Выбор: ")
 
@@ -34,28 +36,58 @@ def main():
 def add_player(names, levels):
     name = input("Введите имя игрока: ")
     level = int(input("Введите уровень игрока: ")) 
+
     names.append(name)
     levels.append(level)
+
     print(f"Игрок {name} успешно добавлен! ")
 
 def display_players(names, levels):
     for i in range(len(names)):
         print(f"{i+1} {names[i]} - {levels[i]}")
+
     print()
 
 def remove_players(names, levels):
     index = int(input("Введите номер игрока для удаления:"))
-    remove_name = names.pop(index)
-    remove_level = levels.pop(index)
+
+    remove_name = names.pop(index - 1)
+    remove_level = levels.pop(index - 1)
+
     print(f"Игрок {remove_name} c уровнем {remove_level} был удалён: ")
 
 def search_players(names, levels):
-    pass
+    search_name = input("Введите имя для поиска: ")
+
+    found = False
+    for i in range(len(names)):
+        if names[i] == search_name:
+            print(f"Найден игрок {i+1} {names[i]} c уровнем {levels}: ")
+            found = True
+
+    if not found: 
+        print("Игрок не найден: ")
 
 def level_up(names, levels):
-    pass
+    player_id = int(input("Введите номер игрока для повышения уровня:"))
+    player_id -= 1
     
+    if player_id < 0 or player_id >= len(names):
+        print("Игрок не найден: ")
+        return
+    
+    increase_level = int(input("На сколько повысить уровень: "))
+    levels[player_id] += increase_level
+
+    print(f"Текущий уровень {names[player_id]} {levels[player_id]}")
+
 def sort_players(names, levels):
-    pass
+    for i in range(len(levels)):
+        for j in range(len(levels)-1-i):
+            if levels[j] > levels[j+1]:
+                levels[j], levels[j+1] = levels[j+1], levels[j]
+                names[j], names[j+1] = names[j+1], names[j]
+
+    print("Сортировка прошла: ")
 
 main()
